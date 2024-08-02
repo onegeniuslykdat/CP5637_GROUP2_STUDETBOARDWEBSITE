68f9fd25b3fbbe261407c589b97b3bf66d339404
Anthony Onyekwere <anthony.udo.onyekwere@outlook.com>
2024-08-02 10:24:06 +1000

    Staatic plugin files deployment

---
+++ wp-content/uploads/wpforms/cache/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head><body>
+<h1>Forbidden</h1>
+<p>You don't have permission to access /wp-content/uploads/wpforms/cache/index.php
+on this server.</p>
+<hr>
+<address>Apache Server at jcubstudentboardgroup2.cloudaccess.host Port 443</address>
+</body></html>
+<!-- Powered by Staatic (https://staatic.com/) -->
